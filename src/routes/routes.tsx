fba240ee709c313d0be3ebb1a7bca5cf8739006e
arifkhan101 <arifkhan230122@gmail.com>
2024-07-06 21:07:42 +0600

    rendered sidebar items based on user role

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,7 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import { routeGenerator } from "../utils/routeGenerator";
 import { adminPaths } from "./admin.routes";
+import { facultyPaths } from "./faculty.routes";
 
 const router = createBrowserRouter([
   {
@@ -18,7 +19,7 @@ const router = createBrowserRouter([
   {
     path: "/faculty",
     element: <App />,
-    children: routeGenerator(adminPaths),
+    children: routeGenerator(facultyPaths),
   },
   {
     path: "/student",
